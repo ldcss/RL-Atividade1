fda024f5eb39b870ad8ce37b0ed24d2cdb0ab657
ldcss <lucass.dcss75@gmail.com>
2025-05-24 18:33:25 -0300

    feat: add filter logic based on category and update sample products

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,35 +1,34 @@
+import { useEffect, useState } from 'react';
+import CategoryFilter from '../../components/Category/Category';
 import Main from '../../components/Main/Main';
 import Navbar from '../../components/Navbar/Navbar';
 import ProductCard from '../../components/ProductCard/ProductCard';
-import { categories } from '../../utils/categoriesData';
 import { products } from '../../utils/productsData';
 
 export const Home = () => {
+  const [selectedCategory, setSelectedCategory] = useState<string | null>(null);
+
+  const filteredProducts = selectedCategory
+    ? products.filter(product => product.category === selectedCategory)
+    : products;
+
+  useEffect(() => {
+    console.log(selectedCategory);
+  }, [selectedCategory]);
+
   return (
     <div className='min-h-screen flex flex-col'>
       <Navbar cartItems={[]} favItems={[]} />
       <Main>
         <div className='flex flex-col items-center justify-center h-full'>
           {/* Seção de categorias */}
-          <div className='mt-8 w-full'>
-            <h2 className='text-2xl font-semibold text-center mb-6'>Navegar por Categorias</h2>
-            <div className='flex flex-wrap justify-center gap-8'>
-              {categories.map(category => (
-                <div
-                  key={category.id}
-                  className='flex flex-col items-center justify-center bg-gray-200 rounded-lg shadow-md p-6 w-40 h-40 hover:bg-gray-300 cursor-pointer'
-                >
-                  <img src={category.image} alt={category.title} className='w-20 h-20 mb-4' />
-                  <p className='text-base font-medium text-gray-700'>{category.title}</p>
-                </div>
-              ))}
-            </div>
-          </div>
+          <CategoryFilter onCategorySelect={setSelectedCategory} selectedCategory={selectedCategory} />
 
           <div className='flex flex-wrap gap-8 justify-center mt-8'>
-            {products.map(product => (
-              <ProductCard key={product.id} product={product} />
-            ))}
+            {filteredProducts.map(product => {
+              console.log(product.title);
+              return <ProductCard key={product.id} product={product} />;
+            })}
           </div>
         </div>
       </Main>
--- src/utils/categoriesData.ts
+++ src/utils/categoriesData.ts
@@ -1,8 +1,46 @@
-export const categories = [
-  { id: 1, title: 'Acessórios', image: '/images/categories/acessorio.webp' },
-  { id: 2, title: 'Componentes', image: '/images/categories/componentes.jpeg' },
-  { id: 3, title: 'Gamer', image: '/images/categories/jogos.jpg' },
-  { id: 4, title: 'Notebooks', image: '/images/categories/notebook.jpg' },
-  { id: 5, title: 'Periféricos', image: '/images/categories/periferico.jpg' },
-  { id: 6, title: 'Celulares', image: '/images/categories/telefone.webp' },
+import type { Category } from '../types/Category';
+
+export const categories: Category[] = [
+  {
+    id: 'accessories',
+    name: 'Acessórios',
+    image: '/images/categories/acessorio.webp',
+    color: 'text-blue-600',
+    bgGradient: 'from-blue-500 to-blue-600',
+  },
+  {
+    id: 'components',
+    name: 'Componentes',
+    image: '/images/categories/componentes.jpeg',
+    color: 'text-pink-600',
+    bgGradient: 'from-pink-500 to-pink-600',
+  },
+  {
+    id: 'gamer',
+    name: 'Gamer',
+    image: '/images/categories/jogos.jpg',
+    color: 'text-green-600',
+    bgGradient: 'from-green-500 to-green-600',
+  },
+  {
+    id: 'notebooks',
+    name: 'Notebooks',
+    image: '/images/categories/notebook.jpg',
+    color: 'text-orange-600',
+    bgGradient: 'from-orange-500 to-orange-600',
+  },
+  {
+    id: 'peripherics',
+    name: 'Periféricos',
+    image: '/images/categories/periferico.jpg',
+    color: 'text-purple-600',
+    bgGradient: 'from-purple-500 to-purple-600',
+  },
+  {
+    id: 'smartphones',
+    name: 'Celulares',
+    image: '/images/categories/telefone.webp',
+    color: 'text-rose-600',
+    bgGradient: 'from-rose-500 to-rose-600',
+  },
 ];
