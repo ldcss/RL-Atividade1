26be8200b4c3101f5a5cfb450bb2b7801c0a0f3e
ldcss <lucass.dcss75@gmail.com>
2025-05-25 23:13:45 -0300

    fix: fix overflow from CartPopup

--- src/components/CartPopup/CartPopup.tsx
+++ src/components/CartPopup/CartPopup.tsx
@@ -77,7 +77,7 @@ const CartPopup = ({
           ) : (
             <>
               {/* Items List */}
-              <div className='flex-1 overflow-y-auto p-4 space-y-4'>
+              <div className='flex-[0.85] overflow-y-auto p-4 space-y-4'>
                 {items.map(item => (
                   <div
                     key={item.id}
